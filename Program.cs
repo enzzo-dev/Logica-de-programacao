4887d01b5bbf7c5fa1e283f4e0c768958dc4e107
Enzzo Lima Alves <enzzolimaalves2013@gmail.com>
2020-11-11 11:10:01 -0300

    Primeiro programa de console em c#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logica_de_programação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui vai o código
+         //Entrada
+         Console.WriteLine("Digite um número:");
+         int valor1 = int.Parse(Console.ReadLine());
+         Console.WriteLine("Digite outro valor:");
+          int valor2 = int.Parse(Console.ReadLine());
+
+          //Processamento
+           int soma = valor1 + valor2;
+         
+          //Exibir
+          Console.WriteLine(" A soma dos valores é: " + soma);
+        }
+    }
+}
