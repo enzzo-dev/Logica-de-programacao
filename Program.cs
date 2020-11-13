957faa90d0fcb7e10a3e79479b18611091d1a820
DEV <dev@info.local>
2020-11-13 10:31:09 -0300

    Desafios e exercicios

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace Logica_de_programação
         static void Main(string[] args)
         {
             //Aqui vai o código
-            //Entrada
+
+            //Aula do dia 11/11
+            
             /*Console.WriteLine("Digite um número:");
             int valor1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite outro valor:");
@@ -21,8 +23,13 @@ namespace Logica_de_programação
                 }
             Console.WriteLine("O resultado da some entre " + valor1 + " e " + valor2 + " é " + soma);
             //Concatenação entre texto e váriaveis*/
+            //Data da aula 11/11
+
+            //Data da aula 12/11
 
             //Entrada de dados
+
+           /*
             Console.WriteLine("Qual o nome do aluno?");
             string nome = Console.ReadLine();  
 
@@ -35,7 +42,6 @@ namespace Logica_de_programação
             Console.WriteLine("Digite a Terceira nota:");
             float nota3 = float.Parse(Console.ReadLine());
 
-
             //Processamento
             float media = (nota1 + nota2 + nota3) / 3;
 
@@ -46,8 +52,99 @@ namespace Logica_de_programação
             } else {
                 Console.WriteLine("O(a) Aluno(a) "+nome+ " está de recuperação e sua média foi: "+media);
             }
-           
-        
+            */
+            //Aula do dia 12/11
+
+           //Aula do 13/11
+
+            /*Console.WriteLine("Qual a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+             if(idade >= 18){
+                Console.WriteLine("Você gostaria de participar do time de CS G0? - Sim ou Não");
+                string resposta = Console.ReadLine();
+                if(resposta == "Sim"){
+                    Console.WriteLine("Compareça a seretaria - Faar com a Sara");
+                } else {
+                    Console.WriteLine("Blz! Passar bem!");
+                }
+             } else{
+                 Console.WriteLine("Você gostaria de participar do time de LOL?");
+                 string resposta = Console.ReadLine();
+                 if( resposta == "Sim"){
+                     Console.WriteLine("Compareça a secretaria - Falar com Juscelino!");
+                 } else {
+                     Console.WriteLine("Muito obrigado!");
+                 }
+             } */
+
+            //Exercicio1
+            Console.WriteLine("Qual a sua idade?");
+            int ano = int.Parse(Console.ReadLine());
+
+            int mes = 12;
+            int dias = 31;
+            int horas = 24;
+            int minutos = 60;
+
+            int idadeMes = mes * ano;
+            int idadeDias = dias * idadeMes;
+            int idadeHoras = horas * idadeDias;
+            int idadeMinutos = minutos * idadeHoras;
+
+            Console.WriteLine(" Sua idade é "+ano+ " anos, "+idadeMes+ " meses, " +idadeDias+ " Dias, " +idadeHoras+ " Horas, " +idadeMinutos+ " Minutos.");
+ 
+
+
+
+            //Exercicio 2
+            Console.WriteLine("Qual o ano de seu nascimento?");
+            int ano2 = int.Parse(Console.ReadLine());
+            int anoAtual = 2020;
+            int semana = 52;
+
+            int idade = anoAtual - ano2;
+            int idadeSemana = idade * semana;
+
+            Console.WriteLine("Você tem " +idade+ " Anos");
+
+            Console.WriteLine("Sua idade em semanas é: " +idadeSemana+ " semanas"); 
+
+            //Exercicio 3
+
+             Console.WriteLine("Qual o seu salário?");
+            float salario = float.Parse(Console.ReadLine());
+            float aumento =  30;
+            float reajuste = (salario * (aumento/100))+salario;
+            if ( salario < 500){
+                Console.WriteLine("Você tem direito a um aumento! E o reajuste do seu salário vai ficar igual a: "+reajuste+ " reais");
+            } else {
+                Console.WriteLine("Você não tem direito ao reajuste de acordo com seu salário!");
+            }
+            
+
+            //Desafio 1
+
+            Console.WriteLine("Digite sua idade");
+            int idadeNatacao = int.Parse(Console.ReadLine());
+
+            string categoria1 = "Infantil-A";
+            string categoria2 = "Infantil-B";
+            string categoria3 = "Juvenil-A";
+            string categoria4 = "Juvenil-B";
+            string categoria5 = "Senior";
+
+            if(idadeNatacao >= 5 && idadeNatacao<=7){
+                System.Console.WriteLine("Você está inserido dentro da categoria: "+categoria1);
+            } else if( idadeNatacao >= 8 && idadeNatacao<=10 ){
+                System.Console.WriteLine("Você está inserido dentro da categoria: " +categoria2);
+            } else if(idadeNatacao >= 11 && idadeNatacao<=13){
+                 System.Console.WriteLine("Você está inserido dentro da categoria: " +categoria3);
+            } else if(idadeNatacao >= 14 && idadeNatacao<=17){
+                 System.Console.WriteLine("Você está inserido dentro da categoria: " +categoria4);
+            } else if(idadeNatacao >= 18){
+                 System.Console.WriteLine("Você está inserido dentro da categoria: " +categoria5);
+            }
+
         }
     }
 }
